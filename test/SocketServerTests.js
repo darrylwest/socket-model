2d91b06f93e5364d47b1657c037f8e6a6cd5bb76
darryl west <darryl.west@RainCitySoftware.com>
2014-06-21 15:23:10 -0700

    added basic instantiation + tests

--- test/SocketServerTests.js
+++ test/SocketServerTests.js
@@ -6,12 +6,40 @@
  */
 var should = require('chai').should(),
     dash = require('lodash' ),
-    SimulationServer = require('../lib/SocketServer');
+    SocketModel = require('../lib/SocketModel' ),
+    SocketServer = require('../lib/SocketServer');
 
 describe('SocketServer', function() {
     'use strict';
 
+    var log = require('simple-node-logger' ).createLogger();
+
+    var createOptions = function() {
+        var opts = {};
+
+        opts.log = log;
+        opts.socketFile = '/tmp/test.sock';
+        opts.removeFileOnStart = true;
+
+        return opts;
+    };
+
     describe('#instance', function() {
-        it('should create an instance of SocketServer');
+        var server = new SocketServer( createOptions() ),
+            methods = [
+                'getSocketFile'
+            ];
+
+        it('should create an instance of SocketServer', function() {
+            should.exist( server );
+            server.should.be.instanceof( SocketServer );
+        });
+
+        it('should have all expected methods by size and type', function() {
+            dash.methods( server ).length.should.equal( methods.length );
+            methods.forEach(function(method) {
+                server[ method ].should.be.a( 'function' );
+            });
+        });
     });
 });
